09d70d20389a734ba9f4360d2bd8a3407b9d6560
Daniel <danielcoc356@gmail.com>
2025-10-23 16:48:52 -0500

    Aca agregue la clase rombo, y sobreescribi la funcion draw

---
+++ Figuras/Rombo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Figuras
+{
+    public class Rombo : Figura
+    {
+        public Rombo(int x, int y, int tam, Color color) : base(x, y, tam, color) { }
+
+        public override void Draw(Graphics g)
+        {
+            var bb = GetBoundingBox();
+            //acomodando los puntos gracias al boundingbox heredado de figura
+            Point pTop = new Point(X, bb.Top);
+            Point pRight = new Point(bb.Right, Y);
+            Point pBottom = new Point(X, bb.Bottom);
+            Point pLeft = new Point(bb.Left, Y);
+            
+            Point[] pts = new Point[] { pTop, pRight, pBottom, pLeft };
+
+            using (Brush b = new SolidBrush(Color))
+            using (Pen p = new Pen(Color.Black, 2))
+            {
+                g.FillPolygon(b, pts);
+                g.DrawPolygon(p, pts);
+            }
+        }
+    }
+}
+
